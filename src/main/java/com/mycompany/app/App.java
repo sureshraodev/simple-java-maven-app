83e05a0aa8da23befa871b7a10836778605c446a
sureshraodev <40878322+sureshraodev@users.noreply.github.com>
2022-05-02 02:33:07 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "This is a test application to test argocd integration!";
 
     public App() {}
 
